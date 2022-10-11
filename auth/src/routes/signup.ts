0f6344da247cecd73b380420db7c14c687461cfd
Nick Olson <naolson.1819@gmail.com>
2022-10-11 15:20:37 -0500

    created initial files for auth service

---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+
+const router = express.Router();
+
+router.post(
+  '/api/users/signup',
+  // use express-validator to validate the request body
+  [
+    body('email').isEmail().withMessage('Email is invalid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters'),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+
+    const { email, password } = req.body;
+
+    console.log('Creating a user...');
+    res.send({});
+  }
+);
+
+export { router as signupRouter };
