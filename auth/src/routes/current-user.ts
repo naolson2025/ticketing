74ffaf734227d1aeea6768eeef5cf40d096cd788
Nick Olson <naolson.1819@gmail.com>
2022-12-17 13:21:41 -0600

    Moved error handling and middlewares to common npm module

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@nao2025tickets/common';
 
 const router = express.Router();
 
