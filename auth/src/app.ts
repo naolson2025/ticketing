70c856291e1100a260191291a870f50e6f3ff8aa
Nick Olson <naolson.1819@gmail.com>
2022-12-18 14:23:45 -0600

    updated imports to import from common module

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -7,8 +7,7 @@ import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
+import { errorHandler, NotFoundError } from '@nao2025tickets/common';
 
 const app = express();
 // traffic is being proxied to our service through ingress-nginx
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { User } from '../models/user';
-import { BadRequestError } from '../errors/bad-request-error';
+import { BadRequestError, validateRequest } from '@nao2025tickets/common';
 import jwt from 'jsonwebtoken';
-import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
