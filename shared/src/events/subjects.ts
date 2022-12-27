0107e242a77bb75439065b44e9b362c645b44396
Nick Olson <naolson.1819@gmail.com>
2022-12-27 15:27:01 -0600

    update

--- shared/src/events/subjects.ts
+++ shared/src/events/subjects.ts
@@ -1,5 +1,9 @@
 // list of all channels in our nats server
+// aka: all event names
 export enum Subjects {
   TicketCreated = 'ticket:created',
   TicketUpdated = 'ticket:updated',
+
+  OrderCreated = 'order:created',
+  OrderCancelled = 'order:cancelled',
 }
